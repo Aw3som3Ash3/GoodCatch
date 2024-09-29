4a1cebbd1e60c80e27436e2f41c9f13aefb7fe05
ChrisBT9 <149310339+ChrisBT9@users.noreply.github.com>
2024-09-28 21:02:30 -0400

    Update7364872

--- Assets/Scripts/MiniGame/FishingMiniGame.cs
+++ Assets/Scripts/MiniGame/FishingMiniGame.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class MiniGame : MonoBehaviour
+using Cinemachine;
+public class FishingMiniGame : MonoBehaviour
 {
+    [SerializeField] CinemachineVirtualCamera virtualCamera;
     //Start Mini Game
     //Detect Win/Loss
     //Output Fish
