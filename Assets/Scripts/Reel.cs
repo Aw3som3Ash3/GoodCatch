58834fe358a29b111acc95eb7675f5a3763235ee
Luckyleol <leolevain@gmail.com>
2024-11-08 16:32:14 -0500

    added basic equipment. needs to  be filled out

---
+++ Assets/Scripts/Reel.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Reel", menuName = "Items/Equipment/Reel", order = 1)]
+public class Reel : Equipment
+{
+    
+}
---
+++ Assets/Scripts/Rod.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Rod", menuName = "Items/Equipment/Rod", order = 0)]
+public class Rod : Equipment
+{
+
+}
