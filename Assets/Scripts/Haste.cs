ea7f847fa1f5097294872a607f0a909a0ecb98a5
Luckyleol <leolevain@gmail.com>
2024-11-15 21:12:52 -0500

    fixed ablility button scaling

---
+++ Assets/Scripts/Haste.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Haste : StatusEffect
+{
+    public override void DoEffect(CombatManager.Turn turn)
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
