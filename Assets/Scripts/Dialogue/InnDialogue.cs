378f2de173b8e71260d6b24977e9d971cb6baf2b
NicolasSpohn <149342232+NicolasSpohn@users.noreply.github.com>
2024-11-15 23:59:05 -0500

    Can't sleep message for the inn with elements in place for sleep message. Getting somewhere?

--- Assets/Scripts/Dialogue/InnDialogue.cs
+++ Assets/Scripts/Dialogue/InnDialogue.cs
@@ -9,14 +9,57 @@ public class InnDialogue : MonoBehaviour
     private Button option1;
     private Button option2;
 
+    GameObject UIElement;
+    
+
+
     private void OnEnable()
     {
+        UIElement = this.gameObject;
         var rootVisualElement = GetComponent<UIDocument>().rootVisualElement;
 
-        dialogue = rootVisualElement.Q<Label>("#ChangeableText");
-        option1 = rootVisualElement.Q<Button>("#Yes");
-        option1 = rootVisualElement.Q<Button>("#No");
+        dialogue = rootVisualElement.Q<Label>("InnSpeak");
+        option1 = rootVisualElement.Q<Button>("Yes");
+        option2 = rootVisualElement.Q<Button>("No");
 
+        //dialogue.text = "Welcome to the inn, would you like to stay?";
+
+        //option2.RegisterCallback<ClickEvent>(ev => DisableUI());
+    }
+
+    private void Start()
+    {
+        DisableUI();
+    }
+
+    private void DisableUI()
+    {
+        UIElement.SetActive(false);
+    }
+    private void ShowUI()
+    {
+        Debug.Log("ShowUI function has been called.");
+        UIElement.SetActive(true);
+    }
+
+    public void DisplayFirstOption()
+    {
+        ShowUI();
         dialogue.text = "Welcome to the inn, would you like to stay?";
+
+        //option1.RegisterCallback<ClickEvent>(ev => DisableUI());
+    }
+
+    public void CantSleepMessage()
+    {
+        ShowUI();
+        StartCoroutine(CantSleepText());
+    }
+
+    private IEnumerator CantSleepText()
+    {
+        dialogue.text = "It is not night time... You can't sleep.";
+        yield return new WaitForSeconds(2.5f);
+        DisableUI();
     }
 }
--- Assets/Scripts/Inn.cs
+++ Assets/Scripts/Inn.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Inn : MonoBehaviour, IInteractable
 {
@@ -9,31 +11,33 @@ public class Inn : MonoBehaviour, IInteractable
     [SerializeField]
     Transform respawnPoint;
     public static Action<Inn> InnVisited;
+
+    public InnDialogue dialogue;
+
     public bool Interact()
     {
+        //dialogue.DisplayFirstOption();
         InnVisited?.Invoke(this);
         if (GameManager.Instance.CurrentTimeOfDay.HasFlag(GameManager.TimeOfDay.Night))
         {
+            //dialogue.ShowUI();
             print("should sleep");
             GameManager.Instance.AdvanceTime(GameManager.TimeOfDay.Dawn);
             GameManager.Instance.RestoreFish();
             return true;
         }
        print("it is not night time could not sleep");
+       dialogue.CantSleepMessage();
        return false;
     }
     public Vector3 GetRepsawnPoint()
     {
         return respawnPoint.position;
     }
+
     public void SleepThroughNight()
     {
 
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
     }
 
     // Update is called once per frame
