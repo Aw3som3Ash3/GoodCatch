e92f81377fa9e2c85df59885b9c664539a8f6cef
Luckyleol <leolevain@gmail.com>
2024-11-15 20:22:00 -0500

    tooltip experimentation

---
+++ Assets/Scripts/AbilityToolTip.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class AbilityToolTip : VisualElement
+{
+    public VisualElement content { get; private set; }
+    public new class UxmlFactory : UxmlFactory<AbilityToolTip, AbilityToolTip.UxmlTraits>
+    {
+
+    }
+    public new class UxmlTraits : UnityEngine.UIElements.UxmlTraits
+    {
+
+    }
+    public AbilityToolTip()
+    {
+        VisualElement root = this;
+        VisualTreeAsset visualTreeAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Prefabs/UI/ToolTip.uxml");
+        visualTreeAsset.CloneTree(root);
+        content = this.Q("content");
+        this.visible = false;
+        this.style.position = Position.Absolute;
+    }
+}
