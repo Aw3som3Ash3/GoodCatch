3df3820eb34250a0687ec5ae6fad3778e8ef5697
Luckyleol <leolevain@gmail.com>
2024-11-29 17:50:05 -0500

    adjusted scroller to not be focused by controller

--- Assets/Scripts/Bestiary.cs
+++ Assets/Scripts/Bestiary.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -31,6 +32,7 @@ public class Bestiary : PausePage
         fishList = this.Q<ListView>("FishList");
         fishLabel = this.Q<Label>("HoveredNameAmount");
         fishPic = this.Q("PngAmount");
+        this.Q("unity-slider").Children().First().focusable = false;
         SetList();
         fishList.selectionChanged += SelectionChanged;
         fishList.itemsChosen += ChoseItem;
