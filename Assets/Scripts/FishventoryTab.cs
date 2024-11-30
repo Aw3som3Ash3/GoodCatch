30cf5ede85c85fcb28c70979372e28b1ad389ffe
Luckyleol <leolevain@gmail.com>
2024-11-29 19:07:55 -0500

    Update FishventoryTab.cs

--- Assets/Scripts/FishventoryTab.cs
+++ Assets/Scripts/FishventoryTab.cs
@@ -36,12 +36,13 @@ public class FishventoryTab : TabbedView
         Debug.Log(m_NumberOfTabs);
         var content = this.Q("unity-content-container");
         tabs =new TabButton[m_NumberOfTabs];
-        for(int i = 0; i < m_NumberOfTabs; i++)
+        for(int i = 1; i <= m_NumberOfTabs; i++)
         {
             //int index = i;
             VisualElement box = new VisualElement();
+            box.name = "box" + i;
             content.Add(box);
-            TabButton tab = new TabButton("Tab "+(i+1), box);
+            TabButton tab = new TabButton("Tab "+i, box);
             tab.RegisterCallback<NavigationSubmitEvent>((x) => { Activate(tab); index = 0; });
             tab.name = "Tab"+i;
             this.AddTab(tab, true);
