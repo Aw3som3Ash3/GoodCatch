4fb04ac1e18c5ca2ae0dba730e7212ec7a9943c6
Luckyleol <leolevain@gmail.com>
2025-01-29 12:57:31 -0500

    Update InputHudTip.cs

--- Assets/Scripts/InputHudTip.cs
+++ Assets/Scripts/InputHudTip.cs
@@ -7,7 +7,7 @@ using UnityEngine.UIElements;
 
 public class InputHudTip : VisualElement
 {
-    
+
     public string inputName { get; set; }
     InputAction inputAction;
     public new class UxmlFactory : UxmlFactory<InputHudTip, UxmlTraits>
@@ -30,7 +30,7 @@ public class InputHudTip : VisualElement
             {
                 ate.ChangeIcon();
             }
-            //ate.SubscribeToInputChange();
+            
         }
     }
 
