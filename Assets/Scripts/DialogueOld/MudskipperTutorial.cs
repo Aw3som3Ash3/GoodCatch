8df7ab0fa385a7e6032d983c9f70114a6187ed83
Luckyleol <leolevain@gmail.com>
2025-02-05 21:54:47 -0500

    fixed a stupid mistake

--- Assets/Scripts/DialogueOld/MudskipperTutorial.cs
+++ Assets/Scripts/DialogueOld/MudskipperTutorial.cs
@@ -35,7 +35,7 @@ public class MudskipperTutorial : NPC
     private void FinishedTutorial_Event()
     {
        
-        if (postTutorialQuest = null)
+        if (postTutorialQuest == null)
         {
             QuestTracker.Instance.AddQuest(postTutorialQuest, true);
         }
