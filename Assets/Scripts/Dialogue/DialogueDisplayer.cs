228370c1002c02c4c4c66d9007bfebfa1d3e3a84
Leo Levain <leolevain@gmail.com>
2025-02-12 23:11:37 -0500

    me dumb dumb fix

--- Assets/Scripts/Dialogue/DialogueDisplayer.cs
+++ Assets/Scripts/Dialogue/DialogueDisplayer.cs
@@ -46,7 +46,7 @@ public class DialogueDisplayer : MonoBehaviour
             UnityEngine.Cursor.visible = false;
             InputManager.EnablePlayer();
             inputs.UI.NextDialogue.Disable();
-            rootVisualElement.Focus();
+            
             inputs.Dispose();
             IsActive = false;
         };
@@ -61,6 +61,7 @@ public class DialogueDisplayer : MonoBehaviour
             option2.text = decisions[1].choice;
             option1.visible = true; 
             option2.visible = true;
+            rootVisualElement.Q<Button>().Focus();
         };
         reader.Start();
 
