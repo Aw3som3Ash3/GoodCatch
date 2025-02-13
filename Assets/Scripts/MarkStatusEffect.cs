ca6dea11cd87189dec8049ee18e79320dbf53a31
Leo Levain <leolevain@gmail.com>
2025-02-12 21:25:05 -0500

    added asset creation for mark effect

--- Assets/Scripts/MarkStatusEffect.cs
+++ Assets/Scripts/MarkStatusEffect.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-
+[CreateAssetMenu(fileName = "Mark", menuName = "Status Effect/Mark", order = 2)]
 public class MarkStatusEffect : DefensiveEffect
 {
 
