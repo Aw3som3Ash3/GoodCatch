7888a687441d272f3fbfbff967bd4e9283a7f7f6
ChrisBT9 <149310339+ChrisBT9@users.noreply.github.com>
2025-03-21 18:12:52 -0400

    Update Leos Help

--- Assets/UI Assets/ViewPopup.cs
+++ Assets/UI Assets/ViewPopup.cs
@@ -8,9 +8,19 @@ namespace PopupTest
     {
         [UnityEngine.Scripting.Preserve]
         public new class UxmlFactory : UxmlFactory<ViewPopup> { }
+
+        private const string styleResource = "UXMLs/PopupWindowStyleSheet";
+        private const string ussPopup = "popup_window";
+        private const string ussPopupContainer = "popup_container";
+
         public ViewPopup()
         {
-            
+            styleSheets.Add(Resources.Load<StyleSheet>(styleResource));
+            AddToClassList(ussPopupContainer);
+
+            VisualElement window = new VisualElement();
+            window.AddToClassList(ussPopup);
+            hierarchy.Add(window);
         }
     }
 }
