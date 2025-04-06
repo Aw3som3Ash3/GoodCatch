daaf1bd27b573e492260ef3119132a62acdb1c61
Leo Levain <leolevain@gmail.com>
2025-04-05 22:45:27 -0400

    increased xp gains

--- Assets/Scripts/CombatManager.cs
+++ Assets/Scripts/CombatManager.cs
@@ -509,7 +509,7 @@ public class CombatManager : MonoBehaviour,IUseDevCommands,ISaveable
             int startLevel = fish.Level;
             foreach (FishMonster enemy in enemyFishes)
             {
-                float xpToAdd = ((float)enemy.Level / fish.Level) * 200;
+                float xpToAdd = ((float)enemy.Level / fish.Level) * 800;
                 fish.AddXp(xpToAdd);
                 deltaXp += xpToAdd;
             }
