7bcf98c8e14ba5eafa065cf4994ce2423cd58d4c
Leo Levain <leolevain@gmail.com>
2025-04-08 20:54:19 -0400

    changed to show npc human name

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -77,7 +77,7 @@ public class NPC : MonoBehaviour, IInteractable
     {
         isTalking = false;
         anim = this.GetComponent<Animator>();
-        npcTitleText.text = name;
+        npcTitleText.text = npcName;
        // var reader = new DialogueReader(dialogue);
         //reader.OnChoiceRequired += NPC_OnChoiceRequired;
         //reader.Next();
