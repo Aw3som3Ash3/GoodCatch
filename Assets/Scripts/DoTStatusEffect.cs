d723e823af550a8d7d5332abef53bb93cf2f17e2
Leo Levain <leolevain@gmail.com>
2025-04-16 22:50:58 -0400

    dot now can scale on special

--- Assets/Scripts/DoTStatusEffect.cs
+++ Assets/Scripts/DoTStatusEffect.cs
@@ -6,21 +6,45 @@ public class DoTStatusEffect : StatusEffect
 {
 
     [SerializeField]
-    float damage;
+    float damage,damageScale;
     [SerializeField]
     protected Element element;
+    
 
     public override void DoEffect(CombatManager.Turn turn)
     {
-        if (damage > 0)
-        {
-            turn.TakeDamage(damage, element, Ability.AbilityType.special);
-        }else if (damage < 0)
-        {
-            turn.Restore(-damage);
-        }
+        
         
     }
+    public override StatusEffectInstance NewInstance(CombatManager.Turn owner)
+    {
+        return new DoTStatusEffectInstance(this, owner);
+    }
 
+
+    public class DoTStatusEffectInstance : StatusEffectInstance
+    {
+        public DoTStatusEffectInstance(StatusEffect effect, CombatManager.Turn owner) : base(effect, owner)
+        {
+            
+
+        }
+
+
+        public override bool DoEffect(CombatManager.Turn turn)
+        {
+
+            if (((DoTStatusEffect)effect).damage > 0)
+            {
+                turn.TakeDamage(((DoTStatusEffect)effect).damage+ (owner.special*0.01f) * ((DoTStatusEffect)effect).damageScale, ((DoTStatusEffect)effect).element, Ability.AbilityType.special);
+            }
+            else if (((DoTStatusEffect)effect).damage < 0)
+            {
+                turn.Restore(-((DoTStatusEffect)effect).damage);
+            }
+
+            return base.DoEffect(turn);
+        }
+    }
 
 }
