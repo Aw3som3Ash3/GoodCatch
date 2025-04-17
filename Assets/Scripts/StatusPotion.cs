e00fde714e7713fe437bc5f64dbbb9a47341aa8d
Leo Levain <leolevain@gmail.com>
2025-04-17 18:44:22 -0400

    combat items now showup tooltips

--- Assets/Scripts/StatusPotion.cs
+++ Assets/Scripts/StatusPotion.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[CreateAssetMenu(fileName = "Status Potion", menuName = "Items/Status Potion", order = 3)]
 public class StatusPotion : Potion
 {
     [SerializeField]
     StatusEffect statusEffect;
+    public StatusEffect StatusEffect { get => statusEffect; }
     protected override void PotionEffect(PlayerTurn turn)
     {
         turn.AddEffects(statusEffect,turn);
